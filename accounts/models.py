4ef770fb6ac8db319391aa9520d3680d4d27fe01
JinHo, Ko <colacan01@hotmail.com>
2025-03-07 08:13:20 +0900

    매장 모델 추가: 매장명, 사업자번호, 전화번호, 주소, 웹사이트 및 매장 대표 필드 포함

--- accounts/models.py
+++ accounts/models.py
@@ -28,3 +28,20 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+class Store(models.Model):
+    name = models.CharField(max_length=100, verbose_name='매장명')
+    business_number = models.CharField(max_length=20, verbose_name='사업자번호')
+    phone_number = models.CharField(max_length=15, verbose_name='전화번호')
+    address = models.TextField(verbose_name='주소')
+    website = models.URLField(null=True, blank=True, verbose_name='웹사이트')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stores', verbose_name='매장 대표')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+    
+    class Meta:
+        verbose_name = '매장'
+        verbose_name_plural = '매장 목록'
